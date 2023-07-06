561eeb93c504f9f7d973bb6afdf52f89b22d9b62
aewols <i0y0j057@gmail.com>
2023-07-06 21:25:25 +0900

    [Setting] Project Setup with Assets and Custom Data Model

---
+++ SwiftUIMapApp/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  SwiftUIMapApp
+//
+//  Created by 이연정 on 2023/07/06.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
